6401a2cc4ed54628aa2c1fbc905dc9f526d90052
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-06-17 13:35:46 -0400

    Commit latest changes

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+// Create Sudoku Board
+function loadBoard() {
+   const element = document.getElementById("grid1");
+   const nodes = getElementsByClassName("item1");
+   document.getElementById("demo").innerHTML = nodes.length;
+}
+
+loadBoard();
+
+
+//<body onload="myFunction()"></body>
