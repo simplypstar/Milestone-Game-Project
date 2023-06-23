60e874564b3fcb6bfb43fb558c77d5548b9e5633
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-06-23 01:50:20 -0400

    Updated board ,  add numbers to input fields.

--- README.md
+++ README.md
@@ -9,4 +9,42 @@ Function to identify the active elementt found on:  https://www.w3schools.com/js
 css : 
  - add internal boarers to each grid box - https://www.smashingmagazine.com/2017/09/css-grid-gotchas-stumbling-blocks/
 
- 
+ Grid-Gap property - https://www.w3schools.com/cssref/tryit.php?filename=trycss_grid-gap
+
+ CSS Grid Gotchas And Stumbling Blocks - https://www.smashingmagazine.com/2017/09/css-grid-gotchas-stumbling-blocks/
+
+ Create a Nested Grid (borders around internal grid) - https://www.quackit.com/css/grid/tutorial/create_a_nested_grid.cfm#:~:text=Grid%20items%20can%20become%20grids,and%20it%20becomes%20a%20grid.
+
+How TO - Animate Buttons - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_buttons_animate3
+
+
+
+Rules for playing Sudoku: 
+OBJECTIVE OF SUDOKU: Fill the 9×9 grid so that each row, column, and 3×3 sub-grid contains numbers 1-9 with no repetition.
+NUMBER OF PLAYERS: 1+ player(s)
+MATERIALS: Pen or pencil, sudoku puzzle
+https://gamerules.com/rules/sudoku/
+
+
+What is Sudoku and what are the rules of this game?
+- Sudoku is a popular logic puzzle with numbers. Its rules are quite simple, so even beginners can
+   handle the simple levels.
+What are the basic rules of Sudoku?
+-   Sudoku grid consists of 9x9 spaces.
+-   You can use only numbers from 1 to 9.
+-   Each 3×3 block can only contain numbers from 1 to 9.
+-   Each vertical column can only contain numbers from 1 to 9.
+-   Each horizontal row can only contain numbers from 1 to 9.
+-   Each number in the 3×3 block, vertical column or horizontal row can be used only once.
+-   The game is over when the whole Sudoku grid is correctly filled with numbers.
+-   There are already many numbers filled in on the grid in the simple Sudoku puzzles. 
+- Therefore it’s not so difficult to cope with the solution if you are familiar with the
+    basic rules. However, in order to solve difficult levels and deal with them quickly, you need to use some tricks and learn advanced Sudoku techniques.
+
+- Our Sudoku guide has a lot of tutorial videos for players of all skill levels: from Sudoku
+ absolute beginners to experts. Watch the videos to check out all the rules, tips and strategies of Sudoku and enjoy the game!
+https://sudoku.com/sudoku-rules/
+
+
+
+
--- assets/index.js
+++ assets/index.js
@@ -1,55 +1,53 @@
 // Create JavaScript to control the Sudoku Board
 
+// Define activeInput as global variable to be used with multipe functions
+var activeInput = "";
 
-
-// get the value stored in the active input field
-function getActiveInputField() {
-    const inputField = document.activeElement.id;
-    document.getElementById("demo").innerHTML = element;
+// Click on an input field to get its Element Id; this input field becomes the ActiveElement (selected element)
+// This elements id name is saved to the activeInput global variable
+// This global variable will be used with each of the buttlon functions
+function getActivePosition() {
+    activeInput = document.activeElement.id;
 }
 
 
-// Define the functions that respond when a button is clicked
+// Define the functions that respond when a button is clicked. 
+// After selecting an input field, press button (1-9) that corresponds with the number (1-9) that will
+//   be entered into the ActiveElement (active input field)
 function button1_Function() {
-    document.getElementById(inputField).value = document.getElementById("button1").value;
-   //alert("Button1 was clicked.");
+    document.getElementById(activeInput).value = document.getElementById("button1").value;
 }
 
 function button2_Function() {
-    document.getElementById("demo2").innerHTML = "Hello World 2";
+    document.getElementById(activeInput).value = document.getElementById("button2").value;
 }
 
 function button3_Function() {
-    document.getElementById("demo3").innerHTML = "Hello World 3";
+    document.getElementById(activeInput).value = document.getElementById("button3").value;
 }
 
 function button4_Function() {
-    document.getElementById("demo4").innerHTML = "Hello World 4";
+    document.getElementById(activeInput).value = document.getElementById("button4").value;
 }
 
 function button5_Function() {
-    document.getElementById("demo5").innerHTML = "Hello World 5";
+    document.getElementById(activeInput).value = document.getElementById("button5").value;
 }
 
 function button6_Function() {
-    document.getElementById("demo6").innerHTML = "Hello World 6";
+    document.getElementById(activeInput).value = document.getElementById("button6").value;
 }
 
 function button7_Function() {
-    document.getElementById("demo7").innerHTML = "Hello World 7";
+    document.getElementById(activeInput).value = document.getElementById("button7").value;
 }
 
 function button8_Function() {
-    document.getElementById("demo8").innerHTML = "Hello World 8";
+    document.getElementById(activeInput).value = document.getElementById("button8").value;
 }
 
 function button9_Function() {
-    document.getElementById("demo9").innerHTML = "Hello World 9";
+    document.getElementById(activeInput).value = document.getElementById("button9").value;
 }
 
 
-// get the value stored in the active input field
-function getActiveInputField() {
-    const inputField = document.activeElement.id;
-    document.getElementById("demo").innerHTML = element;
-  }
